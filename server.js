6416de75e427363b0aba47dca2382629589d06fd
Tim Tanner <timtanner@tanner-companies.com>
2020-08-03 09:13:34 -0500

    Heroku server not working fix

--- server.js
+++ server.js
@@ -7,19 +7,19 @@ const PORT = process.env.PORT || 3000;
 
 app.use(morgan("dev"));
 
-const logger = (req, res, next) => {
-  console.log(`${req.protocol}://${req.get('host')}${req.originalUrl}`);
-  next();
-};
+// const logger = (req, res, next) => {
+//   console.log(`${req.protocol}://${req.get('host')}${req.originalUrl}`);
+//   next();
+// };
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
-app.use(logger);
+// app.use(logger);
 
-// mongoose.connect(process.env.MONGODB_URI || "mongodb://ttanner44:2644zzzz@ds053459.mlab.com:53459/heroku_v8sp9xb8", { useNewUrlParser: true, useFindAndModify: false, });
-const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/workout";
-mongoose.connect(MONGODB_URI, {useNewUrlParser: true, useFindAndModify: false, });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://ttanner44:2644zzzz@ds053459.mlab.com:53459/heroku_v8sp9xb8", { useNewUrlParser: true, useFindAndModify: false, });
+// const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/workout";
+// mongoose.connect(MONGODB_URI, {useNewUrlParser: true, useFindAndModify: false, });
 
 require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
